a6f07623dd27b26aeb9098c69f0fcfd461aaeba4
azrap <azraap@gmail.com>
2019-11-11 13:59:42 -0800

    everything except dequeue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,17 +3,21 @@ sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
+
+
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage=DoublyLinkedList(None)
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         pass
+        # return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.length
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,35 @@
-from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
 class Stack:
-    def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+    def __init__(self, node=None):
+        # self.head = node
+        # self.tail = node
+        # self.length = 1 if node is not None else 0
+        self.storage=DoublyLinkedList(None)
 
     def push(self, value):
-        pass
+        # pass
+        self.storage.add_to_tail(value)
+        # new_node=ListNode(value,None,None)
+        # self.length+=1
+        # if not self.head and not self.tail:
+        #     self.head=new_node
+        #     self.tail=new_node
+        # else:
+        #     new_node.prev=self.tail
+        #     self.tail.next=new_node
+        #     self.tail=new_node
+        
 
     def pop(self):
-        pass
+        return self.storage.remove_from_tail()
+    
+        
 
     def len(self):
-        pass
+        # print(self.storage)
+        # print(self.storage.length)
+        return self.storage.length
+
