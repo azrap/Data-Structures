4dc926cc479b566fac2e0d6e965614fabb28ad00
azrap <azraap@gmail.com>
2019-11-12 15:57:34 -0800

    lru cache is... on its way

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,33 +1,71 @@
+from doubly_linked_list import *
+
+
 class LRUCache:
     """
-    Our LRUCache class keeps track of the max number of nodes it
-    can hold, the current number of nodes it is holding, a doubly-
-    linked list that holds the key-value entries in the correct
-    order, as well as a storage dict that provides fast access
+    Our LRUCache class keeps track of:
+    -  the max number of nodes it can hold.
+    - the current number of nodes it is holding
+    -  a doubly-linked list that holds the key-value entries in the correct
+    order
+    - as well as a storage dict that provides fast access
     to every node stored in the cache.
+    - Least recently used list 
+    - once it reaches limit, it deletes the least recently used.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit=limit
+        self.dll=DoublyLinkedList()
+        self.storage={}
+        self.size=0
+
 
     """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
-    such that the pair is considered most-recently used.
+    - Retrieves the value associated with the given key. 
+    - Also needs to move the key-value pair to the tail of the order
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        print('storage: ', self.storage)
+        print('dll: ', self.dll)
+        # print('HI!')
+        if key in self.storage:
+            value=self.storage[key]
+            
+
+            self.dll.move_to_end((key,value))
+            return value
+        return None
+
 
     """
-    Adds the given key-value pair to the cache. The newly-
+    Adds the given key-value pair to the cache. 
+    The newly-
     added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
+    entry in the cache. 
+    - If the cache is already at max capacity
     before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
+    cache needs to be removed to make room. 
+    - Additionally, in the case that the key already exists in the cache, we simply
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        #if the limit has been reached:
+        if self.size == self.limit:
+            oldest=self.dll.head.value
+            k = oldest[0] #get the key from the head, assuming key/value is being stored as a tuple
+            del self.storage[k] #remove the key/value from the dict
+            self.dll.remove_from_head() #remove the oldest item from the head
+            self.size-=1 #decrease the size by 1
+ 
+        #if key is in the cache, delete the node so you can over
+        if key in self.storage:
+            self.dll.delete((key,value))
+
+        self.storage[key]=value
+        self.dll.add_to_tail((key,value))
+        print('is there a head?', self.dll.head.value)
+        self.size+=1
